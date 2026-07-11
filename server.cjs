fddd887a7fb370c73804f370999e9424c6afb7d4
denghuacc <denghuacc@gmail.com>
2026-07-11 10:16:33 +0800

    chore: update dependencies and tooling

--- server.cjs
+++ server.cjs
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "dist")));
 
 // 处理前端路由
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
